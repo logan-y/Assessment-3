ace1efa4c713268e5688e92faf97c30f1751a305
Logan Young <logany@att.net>
2021-10-31 21:29:52 -0500

    almost finished! Happy Halloweeen!

--- about.js
+++ about.js
@@ -1,25 +1,17 @@
 console.log("hello world");
 
-
-
 function handleSubmit(evt) {
-	evt.preventDefault();
-	
-	console.log('form submit');
+	evt.preventDefault
+	alert("form has been submitted successfully!!")
+	console.log('form submitted tho');
 }
-
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
-//Part 1: Edit HTML
-/*Change the following things in home.html.
 
-Change the image of the Devmountain logo to an image of either a rubber duck or a cat of your choice.
+document.getElementById("saePics").addEventListener("mouseover", mouseOver);
 
-Add another paragraph somewhere on the page. The content of the paragraph should be a message welcoming the user to the website.
-
-Add a bulleted list (ul) of compliments to the user, with at least 3 bullet points.
-
-Add a form input to the “Contact Us” form that asks the user how they heard about this website. They should be able to check boxes that include “Google”, “A friend”, or “Other”
-*/
+function mouseOver() {
+  alert("The quick mouse jumps over the lazy naked cat...You're awesome!");
+}
---
+++ profile.js
@@ -0,0 +1,6 @@
+//In profile.js, create event listeners that check for a click on each of these buttons. If a user clicks on “My Favorite Color”, it should alert the user of your favorite color, and so on.
+
+document.querySelector("#color").addEventListener('click', () => alert("My fav color is a nice soft shade of dark green. Or any blue."))
+document.querySelector("#place").addEventListener('click', () => alert("My favorite place is a nice spot on the San Marcos River between exit 205 and 206 on I-35."))
+document.querySelector("#ritual").addEventListener('click', () => alert("My favorite ritual might be the Bullet Ant gloves of the Satere-Mawe tribe..."))
+
